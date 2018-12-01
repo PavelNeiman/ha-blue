da5fab9a006d273dfe73990c25123679badd8016
Pavel <bonyown2012@gmail.com>
2018-12-01 20:14:48 +0300

    gulp setting <3

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
 
-gulp.task('hello', function(){
-    console.log('hi!');
+
+gulp.task('sass', function(){
+    return gulp.src('app/sass/**/*.+(scss|sass)')    //исходник
+        .pipe(sass())                        //команда
+        .pipe(gulp.dest('app/css'))          //результирующая директория
 });
+
+
+gulp.task('watch', function(){
+    gulp.watch('app/sass/**/*.+(scss|sass)', ['sass'])
+})
