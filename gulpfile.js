cfb8f3c384b3ca92bf83901410661d2f05e1520f
Pavel <bonyown2012@gmail.com>
2018-12-01 22:39:30 +0300

    more plugins

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,11 @@ var gulp            = require('gulp'),
     concat          = require('gulp-concat'),
     uglify          = require('gulp-uglifyjs'),
     cssnano         = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
-    rename          = require('gulp-rename'); // Подключаем библиотеку для переименования файлов
+    rename          = require('gulp-rename'),
+    del             = require('del'),
+    imagemin        = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+    pngquant        = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png // Подключаем библиотеку для удаления файлов и папок; // Подключаем библиотеку для переименования файлов
+    cache       = require('gulp-cache'); // Подключаем библиотеку кеширования
     
 
 
@@ -51,3 +55,43 @@ gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function () {
     gulp.watch('app/js/***.js', browserSync.reload);
 
 })
+
+
+gulp.task('clean', function() {
+    return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+
+gulp.task('img', function() {
+    return gulp.src('app/img/**/*') // Берем все изображения из app
+        .pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+});
+
+
+gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
+
+    var buildCss = gulp.src([ // Переносим CSS стили в продакшен
+        'app/css/main.css',
+        'app/css/libs.min.css'
+        ])
+    .pipe(gulp.dest('dist/css'))
+
+    var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
+    .pipe(gulp.dest('dist/fonts'))
+
+    var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
+    .pipe(gulp.dest('dist/js'))
+
+    var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
+    .pipe(gulp.dest('dist'));
+
+});
+
+
+// Здесь, присваивая переменным какие-либо действия, мы их выполняем. Таким образом можно выполнять мультизадачные таски. Можно и не присваивать, но мы сделаем так, ибо красивше.
+
